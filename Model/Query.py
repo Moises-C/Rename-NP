55ca35da90e49b308260303af6975814a9ad23d6
SISTEMASIV <SISTEMASIV@CAR1742.mx>
2026-03-24 17:23:00 -0600

    Se cambia fecha de obtencion de operaciones

--- Model/Query.py
+++ Model/Query.py
@@ -6,9 +6,8 @@ class Query:
         return """ 
         SELECT num_refe 
         FROM SAAIO_PEDIME
-        WHERE SAAIO_PEDIME.DIA_PAGO >= '2026-01-01'
+        WHERE SAAIO_PEDIME.DIA_PAGO >= '2026-04-01'
             AND SAAIO_PEDIME.FIR_PAGO IS NOT null
             AND SAAIO_PEDIME.FIR_ELEC IS NOT NULL
             AND SAAIO_PEDIME.CVE_IMPO = '3273'
         """
-        
