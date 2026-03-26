d7d5ea5be74c6222711810fd5be9a34653b67261
SISTEMASIV <SISTEMASIV@CAR1742.mx>
2026-03-26 17:53:44 -0600

    Se cambia obtencion de registros

---
+++ Model/ApiRest.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class API:
+    
+    json_client = None
+    response = None
+    URL = {"report": "http://200.66.88.166:5010/report",
+           "letter": "http://200.66.88.166:5010/letter",
+           "update": "http://200.66.88.166:5010/report/update",
+           "catalog": "http://200.66.88.166:5010/KPI/preload",
+           "list_report": "http://200.66.88.166:5010/list_reports"
+    }
+    HEADERS = {"X-API-KEY" : "17d6a5fdd4e94e251238b42e1e8dd5c7dcb8c8702b43ece2b9f630f6b92e3b2f"}
+
+    def handle_response(self, url):
+        try:
+            response = requests.post(self.URL[url], json=self.json_client, headers=self.HEADERS)
+            if response.status_code == 200:
+                self.response = response.json()
+            else:
+                self.response = response.status_code
+        except BaseException as e:
+            self.response = e
+            return e
+
+    def get(self, url):
+        try:
+            response = requests.get(self.URL[url], headers=self.HEADERS)
+            if response.status_code == 200:
+                self.response = response.json()
+            else:
+                self.response = response.status_code
+        except BaseException as e:
+            self.response = e
+            return e
+    
+    def get_json_client(self):
+        return self.json_client
+
+    def set_json_client(self, value):
+        self.json_client = value
+
+    def get_response(self):
+        return self.response
+
+    def set_response(self, value):
+        self.response = value
